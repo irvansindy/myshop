cbd9cc5cd455fc2fa1b3404fa98a914b65cab376
irvansindy <irvanmuhammad22@gmail.com>
2020-07-26 19:32:50 +0700

    init commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header/>
+	<Carousel/>
+	<Product/>
+	<Instagram/>
+	<Partner/>
+	<Footer/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+import Carousel from '@/components/Carousel.vue'
+import Product from '@/components/Product.vue'
+import Instagram from '@/components/Instagram.vue'
+import Partner from '@/components/Partner.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  name: 'Home',
+  components: {
+	Header,
+	Carousel,
+	Product,
+	Instagram,
+	Partner,
+	Footer,
+  }
+}
+</script>
